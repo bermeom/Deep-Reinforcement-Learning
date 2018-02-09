2686cb397a03a44e8c753edb867b53cff75de008
Nilo Freitas <nilo.freitasr@gmail.com>
2018-02-09 14:38:08 -0200

    Update script0

--- reinforcement/script0.sh
+++ reinforcement/script0.sh
@@ -1,2 +1,2 @@
-python3 execute.py source_pygame_catch player_dql_bayesian --load pygame_catch_dql_bayesian_2A --run
+python3 execute.py source_pygame_catch player_dql_bayesian_2A --load pygame_catch_dql_bayesian_2A --run
 
